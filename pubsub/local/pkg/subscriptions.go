52e73c32892fbe5edae7d6d76ae681f5b021550b
David López <not4rent@gmail.com>
2018-06-04 10:40:15 +0200

    Add pubsub functional memory implementation

---
+++ pubsub/local/pkg/subscriptions.go
@@ -0,0 +1,22 @@
+package pkg
+
+func ackSubscription() {
+}
+
+func createSubscription() {
+}
+
+func deleteSubscription() {
+}
+
+func getSubscription() {
+}
+
+func pullSubscription() {
+}
+
+func modifyAckSubscription() {
+}
+
+func listSubscriptions() {
+}
